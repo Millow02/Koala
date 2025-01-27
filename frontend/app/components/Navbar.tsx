f03d7801960b750296aa804918630b2729928c8e
Scott McDonald <smcdo0211@gmail.com>
2025-01-27 17:44:38 -0500

    Fixed navbar and index data handling (switched to props instead of fetching user twice)

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -2,55 +2,19 @@
 import { Link, useOutletContext, useRevalidator } from "@remix-run/react";
 import { SupabaseClient, User } from "@supabase/auth-helpers-remix";
 import { useEffect, useState } from "react";
-import { Database } from "~/types/supabase";
 
-export default function Navbar() {
-    const { supabase } = useOutletContext<{ supabase: SupabaseClient<Database> }>()
-    const revalidator = useRevalidator();
+interface NavbarProps {
+  isSignedIn: boolean;
+  user: User | null;
+}
 
-    const [user, setUser] = useState<User | null>(null);
-    
-    useEffect(() => {
-        const fetchUser = async () => {
-        const { data, error } = await supabase.auth.getUser();
-    
-        if (error) {
-            console.error("Error fetching user:", error.message);
-        } else {
-            setUser(data?.user || null);
-        }
-        };
-    
-        fetchUser();
-    }, []);
-
-
-    useEffect(() => {
-        const { data: subscription } = supabase.auth.onAuthStateChange((event, session) => {
-            if (session?.user) {
-            setUser(session.user); 
-            } else {
-            setUser(null); 
-            }
-            revalidator.revalidate(); 
-        });
-    
-        return () => {
-            subscription.subscription.unsubscribe(); 
-        };
-        }, [supabase, revalidator]);
-
-  const signOut = () => {
-    supabase.auth.signOut();
-  };
-
-
+export default function Navbar({ isSignedIn, user }: NavbarProps) {
   return (
-    <nav className="flex items-center justify-between p-4 bg-gray-800 text-white shadow-md">
+    <nav className="flex items-center justify-between p-4 bg-neutral-800 text-white shadow-md">
       <div className="flex items-center space-x-10">
         <Link
           to="/"
-          className="text-2xl font-bold hover:text-gray-300 transition-colors"
+          className="text-2xl font-bold hover:text-neutral-300 transition-colors"
         >
           Koala
         </Link>
@@ -59,16 +23,18 @@ export default function Navbar() {
       <div>
         {!user ? (
           <Link to="/sign-in">
-            <button className="bg-blue-500 text-white px-6 py-3 rounded-full shadow-md hover:bg-blue-600 hover:scale-105 transition-transform focus:outline-none focus:ring-2 focus:ring-blue-400">
-        Sign In
-      </button>
+            <button className="bg-pink-500 border-e-pink-500 text-white px-6 py-3 rounded-lg shadow-md hover:bg-pink-600 hover:scale-105 transition-transform focus:outline-none focus:ring-2 focus:ring-pink-400">
+              Sign In
+            </button>
           </Link>
         ) : (
-        <button className='bg-blue-500 text-white px-6 py-3 rounded-full shadow-md hover:bg-blue-600 hover:scale-105 transition-transform focus:outline-none focus:ring-2 focus:ring-blue-400' onClick={signOut}>Sign out</button>
-            
+          <Link to="dashboard">
+            <button className="text-base bg-pink-500 rounded-lg p-2 focus:outline-none">
+              Dashboard
+            </button>
+          </Link>
         )}
       </div>
     </nav>
   );
 }
-
