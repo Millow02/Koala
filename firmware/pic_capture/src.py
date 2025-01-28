681a7b38dab2907c9832b3ba2db65dde2f0d2962
angewoah <areoligio@gmail.com>
2025-01-27 19:01:12 -0500

    feat(pic_cap): skeleton
    - Still have GPIO setup error on rising edge detection

--- firmware/pic_capture/src.py
+++ firmware/pic_capture/src.py
@@ -1,5 +1,21 @@
+import RPi.GPIO as GPIO  
+
+
 def main():
-    print("Hello from pic-capture!")
+    GPIO.setmode(GPIO.BOARD)  
+      
+    GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+
+    def camera_capture_callback(channel):  
+        pass
+      
+    # We might not need to add debounce as the IO tend to be quick ?
+    # TODO: Do some research into this
+    GPIO.add_event_detect(13, GPIO.RISING, callback=camera_capture_callback)  
+      
+    GPIO.cleanup()           # clean up GPIO on normal exit  
+
+    print("pic_capture service has shutdown")
 
 
 if __name__ == "__main__":
