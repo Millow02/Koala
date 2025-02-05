b869a6db6d0d662870f3065b0ff15077ec5e2879
Niraj <nirajpateln16@gmail.com>
2025-02-05 14:48:45 -0500

    Path changes

--- c_recognition.py
+++ c_recognition.py
@@ -2,11 +2,7 @@ import cv2
 import easyocr
 import matplotlib.pyplot as plt
 
-<<<<<<< HEAD
-image_path = "C:/Users/niraj/OneDrive/Desktop/Koala/img/car1_out_cropped_zoomed.jpg"
-=======
 image_path = "C:/Users/niraj/OneDrive/Desktop/Koala/img/"
->>>>>>> e2d5095275ea7a46040c1b4b122b89c09db5e55b
 img = cv2.imread(image_path)
 
 reader = easyocr.Reader(['en'],gpu=False)
--- c_segmentation_picture.py
+++ c_segmentation_picture.py
@@ -3,11 +3,7 @@ import cv2
 import os
 
 # Use the absolute path to the image file
-<<<<<<< HEAD
 image_path = 'C:/Users/niraj/OneDrive/Desktop/Koala/img/car5.jpg'
-=======
-image_path = "C:/Users/niraj/Desktop/Koala/c44d319c-car_504.jpg"
->>>>>>> e2d5095275ea7a46040c1b4b122b89c09db5e55b
 output_image_path = '{}_out.jpg'.format(os.path.splitext(image_path)[0])
 
 # Debugging information
