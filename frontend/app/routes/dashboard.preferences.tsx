5074d8d78892619d3f023713228f1f2561a9b28b
Angewoah <areoligio@gmail.com>
2025-02-23 15:35:28 -0500

    visual changes to preferences

--- frontend/app/routes/dashboard.preferences.tsx
+++ frontend/app/routes/dashboard.preferences.tsx
@@ -8,6 +8,9 @@ import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { SupabaseClient, User } from "@supabase/auth-helpers-remix";
 import { useEffect, useState } from "react";
 import { Database } from "~/types/supabase";
+import { UserIcon } from "@heroicons/react/24/solid";
+import { PencilIcon } from "@heroicons/react/24/outline";
+import { Pen } from "lucide-react";
 
 type ContextType = {
   user: User;
@@ -116,57 +119,85 @@ export default function Preferences() {
       <div className="w-full px-12 py-4">
         <div className=" items-center mb-8">
           <h1 className="text-3xl font-bold mb-2">Preferences</h1>
-          <p>Manage your preferences and contact information</p>
-          <div className="flex flex-col w-3/5 pl-6 mt-8 p-6 rounded-lg border-neutral-600 border-2" style={{ backgroundColor: "#333842" }}>
-            <div className="mb-4">
-              <label className="block text-m font-medium text-white mb-1">
-                First Name
-              </label>
-              <input
-                type="text"
-                value={firstName}
-                onChange={(e) => handleChange(setFirstName, e.target.value)}
-                className="w-full rounded-lg px-3 py-2  bg-slate-600"
-                placeholder="Enter first name"
-              />
+          <hr className="border-pink-500 border-1 my-6" />
+
+          <div className="mb-14">
+            <h2 className="text-2xl font-semibold text-white">Profile Picture</h2>
+              <div className="flex flex-col w-3/5 pl-6 mt-5 p-6 rounded-lg border-neutral-600 border-2" style={{ backgroundColor: "#333842" }}>
+                <div className="flex">
+                  <UserIcon className="h-20 w-20 text-white border rounded-full bg-slate-600" />
+                  <button className="flex items-center text-white bg-pink-500 px-4 rounded-lg my-4 mx-8 font-semibold hover:scale-105 transition-transform duration-300 hover:text-gray-600">
+                    Upload Picture
+                    <Pen className="h-6 w-6 ml-4" />
+                  </button>
+                  <div className="mt-7 ml-20">Must be JPEG or PNG and cannot exceed 10MB.</div>
+                </div>
+              </div>
+          </div>
+
+          <div className="mb-14"> 
+            <h2 className="text-2xl font-semibold text-white">Profile Settings</h2>
+            <div className="flex flex-col w-3/5 pl-6 mt-5 p-6 rounded-lg border-neutral-600 border-2" style={{ backgroundColor: "#333842" }}>
+              <div className="mb-10 flex">
+                  <label className="min-w-40 text-lg font-medium text-white mt-2">
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => handleChange(setFirstName, e.target.value)}
+                    className="w-full rounded-lg px-3 py-2  bg-slate-600 font-medium"
+                    placeholder="Enter first name"
+                  />
+                </div>
+                <div className="flex">
+                  <label className="min-w-40 text-lg font-medium text-white mt-2">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => handleChange(setLastName, e.target.value)}
+                    className="w-full rounded-lg px-3 py-2 bg-slate-600 font-medium"
+                    placeholder="Enter last name"
+                  />
+                </div>
             </div>
-            <div className="mb-4">
-              <label className="block text-m font-medium text-white mb-1">
-                Last Name
-              </label>
-              <input
-                type="text"
-                value={lastName}
-                onChange={(e) => handleChange(setLastName, e.target.value)}
-                className="w-full rounded-lg px-3 py-2 bg-slate-600"
-                placeholder="Enter last name"
-              />
+          </div>
+
+
+
+          <div className="mb-14">
+            <h2 className="text-2xl font-semibold text-white">Contact Information</h2>
+            <div className="flex flex-col w-3/5 pl-6 mt-5 p-6 rounded-lg border-neutral-600 border-2" style={{ backgroundColor: "#333842" }}>
+              <div className="mb-10 flex">
+                <label className="min-w-40 text-lg font-medium text-white mt-2">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => handleChange(setEmail, e.target.value)}
+                  className="w-full rounded-lg px-3 py-2 bg-slate-600 font-medium"
+                  placeholder="Enter email"
+                />
+              </div>
+              <div className="flex">
+                <label className="min-w-40 text-lg font-medium text-white mt-2">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  value={phoneNumber}
+                  onChange={(e) => handleChange(setPhoneNumber, e.target.value)}
+                  className="w-full rounded-lg px-3 py-2 bg-slate-600 font-medium"
+                  placeholder="Enter phone number"
+                />
+              </div>
             </div>
-            <div className="mb-4">
-              <label className="block text-m font-medium text-white mb-1">
-                Email
-              </label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => handleChange(setEmail, e.target.value)}
-                className="w-full rounded-lg px-3 py-2 bg-slate-600 placeholder-gray-400"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-m font-medium text-white mb-1">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                value={phoneNumber}
-                onChange={(e) => handleChange(setPhoneNumber, e.target.value)}
-                className="w-full rounded-lg px-3 py-2 bg-slate-600"
-                placeholder="Enter phone number"
-              />
-            </div>
-            {isModified && (
+          </div>
+
+        {isModified && (
               <div className="flex space-x-4">
                 <button
                   onClick={handleUndo}
@@ -184,7 +215,6 @@ export default function Preferences() {
                 
               </div>
             )}
-        </div>
       </div>
     </div>
     {popupVisible && (
