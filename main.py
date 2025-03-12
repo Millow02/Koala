1fd94449a0660ea4c04b4f6d90d95acb7d389269
Niraj <nirajpateln16@gmail.com>
2025-03-12 12:27:52 -0400

    changes

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from Segmentation_Test import LicensePlateSegmentation
 import os 
 
 # Paths to the models
-localization_model_path = 'C:/Users/niraj/Desktop/Koala/localization_model.pt'
-segmentation_model_path = 'C:/Users/niraj/Desktop/Koala/best.pt'
+localization_model_path = 'C:/Users/niraj/OneDrive/Desktop/Koala/localization_model.pt'
+segmentation_model_path = 'C:/Users/niraj/OneDrive/Desktop/Koala/best.pt'
 
 # Paths to the images
-image_path = 'C:/Users/niraj/Desktop/Koala/img/images.jpg'
+image_path = 'C:/Users/niraj/OneDrive/Desktop/Koala/img/images.jpg'
 output_image_path = '{}_segmented.jpg'.format(os.path.splitext(image_path)[0])
 
 # Create instances of the classes
