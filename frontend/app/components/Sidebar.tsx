d2465ed2c2acba840691a64c7031fa13ee7b356d
Angewoah <areoligio@gmail.com>
2025-03-17 13:25:17 -0400

    revert sidebar to only show admin pages

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -243,7 +243,7 @@ const Sidebar = () => {
                 </div>
               </>
             )}
-            {userRole == "admin" && (
+            {userRole == "client" && (
               <>
                 <div className="border border-neutral-600 border-l-0 border-r-0 border-b-0 pl-4 pt-3">
                   <h2 className="text-base font-semibold text-neutral-500">
@@ -284,7 +284,7 @@ const Sidebar = () => {
                 <Cog6ToothIcon className="h-6 w-6 inline-block mr-2" />
                 Preferences
               </Link>
-              {userRole == "admin" && (
+              {userRole == "client" && (
                 <>
                   <Link
                     to={`/dashboard/vehicles`}
